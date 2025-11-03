fc4f228c18392f124a05622700bc59fd58648372
Guillaume Pichot <guillaume.pichot@sedex.com>
2025-11-03 15:36:14 +0000

    Added test to ensure a bad request is returned when creating a user without all required data

--- src/test/java/com/bank/eagle/api/V1ApiImplTest.java
+++ src/test/java/com/bank/eagle/api/V1ApiImplTest.java
@@ -9,25 +9,38 @@ class V1ApiImplTest {
     @Test
     void createNewUser() {
         given()
-            .contentType("application/json")
-            .body("""
-            {
-              "name": "John Doe",
-              "address": {
-                "line1": "Flat 11 The Boathouse",
-                "line2": "36 Edge Row ",
-                "line3": "Ground floor",
-                "town": "London",
-                "county": "UK",
-                "postcode": "E5 6FD"
-              },
-              "phoneNumber": "0987654321",
-              "email": "john.doe@email.com"
-            }
-            """)
-        .when()
-            .post("/api/v1/users")
-        .then()
-            .statusCode(201);
+                .contentType("application/json")
+                .body("""
+                        {
+                          "name": "John Doe",
+                          "address": {
+                            "line1": "Flat 11 The Boathouse",
+                            "line2": "36 Edge Row ",
+                            "line3": "Ground floor",
+                            "town": "London",
+                            "county": "UK",
+                            "postcode": "E5 6FD"
+                          },
+                          "phoneNumber": "0987654321",
+                          "email": "john.doe@email.com"
+                        }
+                        """)
+                .when()
+                .post("/api/v1/users")
+                .then()
+                .statusCode(201);
+    }
+
+    @Test
+    void createNewUserWithoutAllRequiredData() {
+        given()
+                .contentType("application/json")
+                .body("""
+                        {}
+                        """)
+                .when()
+                .post("/api/v1/users")
+                .then()
+                .statusCode(400);
     }
 }
